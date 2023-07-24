e0608180ae8edca83e16c39db27a8cd0fde8a477
SinclairPythonAkoto <sinclair.python@gmail.com>
2023-07-25 00:47:34 +0100

    test blog

--- stop_search/app.py
+++ stop_search/app.py
@@ -1,36 +1,43 @@
 # frontend
 from flask import Flask, render_template, request, redirect, url_for
 import requests
+import datetime
 
 app = Flask(__name__)
 backend_url = "http://localhost:8000"
 
 class Post:
-    def __init__(self, title, content):
+    def __init__(self, title, content, date):
         self.title = title
         self.content = content
+        self.date = date
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        # get info form form file
         title = request.form["title"]
         content = request.form["content"]
-        # create form data into request clss object
-        post = Post(title=title, content=content)
-        # send the request clss object to backend
+        use_current_time = request.form.get("use_current_time")  # Check if checkbox is selected
+        date = request.form["date"] if not use_current_time else datetime.datetime.now().strftime("%Y-%m-%dT%H:%M")
+        post = Post(title=title, content=content, date=date)
         response = requests.post(f"{backend_url}/posts/", json=post.__dict__)
-        # send user back to FE
         return redirect(url_for("index"))
     else:
-        # get request from backend
         response = requests.get(f"{backend_url}/posts/")
-        # turn the response into json object
         posts_data = response.json()
-        # unpack data into list comprehension
         posts = [Post(**post) for post in posts_data]
-        # pass list comp to frontend for display
-        return render_template("index.html", posts=posts)
+        return render_template("index.html", posts=posts[::-1])
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
+    
+"""
+main page - dashboard/homepage
+victim report page
+witness report page
+know my rights
+map --> map partial witness/victim reports (follium)
+
+"""
--- stop_search/main.py
+++ stop_search/main.py
@@ -8,21 +8,85 @@ app = FastAPI()
 
 # MongoDB connection
 client = MongoClient("mongodb://localhost:27017/")
-db = client["blog"]
-collection = db["posts"]
+db = client["stop_search"]
+report_collection = db["reports"]
 
 class Post(BaseModel):
     title: str
     content: str
+    date: str = None # Date will be empty string format
 
 @app.post("/posts/", response_model=Post)
 def create_post(post: Post):
     post_data = post.dict()
-    post_id = collection.insert_one(post_data).inserted_id
+    post_id = report_collection.insert_one(post_data).inserted_id
     return {**post_data, "_id": str(post_id)}
 
 @app.get("/posts/", response_model=List[Post])
 def get_posts():
-    posts_data = collection.find()
+    posts_data = report_collection.find()
     return [Post(**post) for post in posts_data]
 
+
+
+"""
+class LocationInput(BaseModel):
+    latitude: str
+    longitude: str
+    address: str
+    country: str
+
+
+class PoliceBadgeInfo(BaseModel):
+    badge_number: str
+    officer: str
+
+
+class VictimReports(BaseModel):
+    report_type: str  # "victim"
+    date: Union[str, datetime]  # "get_date" | datetime
+    location: Union[str, LocationInput]  # "get_location" | LocationInput
+    victims_involved: str  # 1 | 2 | 3 ... 10+
+    reason: str  # I don't know | drugs | weapon | stolen property | something to commit a crime | suspect serious violence | carrying a weapon or have used one | in specific location/area
+    visible_police: str  # 1-2 | 3-4 | 5-6 | 6+
+    type_of_search: str  # moderate | aggressive
+    police_badge: List[PoliceBadgeInfo]  # PoliceBadgeInfo
+    outcome: str  # unknown | ongoing | resolved | no further action
+    age: str  # prefer not to say | below 15 | 15-17 | 18-24 | 25-30 | 31-35 | 35+
+    sex: str  # prefer not to say | male | female | non-binary | trans
+    race: str  # prefer not to say | black | white | arab | south asain | east asain
+    notes: str  # (optional)
+    supporting_evidence: List[UploadFile]  # (optional)
+    email: str  # (optional) - hashed for security
+
+
+class WitnessReports(BaseModel):
+    report_type: str  # "witness"
+    relation: str  # known | unknown
+    date: Union[str, datetime]  # get_date | datetime
+    location: Union[str, LocationInput]  # get_location | LocationInput
+    victims_involved: str  # 1 | 2 | 3 ... 10+
+    reason: str  # I don't know | drugs | weapon | stolen property | something to commit a crime | suspect serious violence | carrying a weapon or have used one | in specific location/area
+    visible_police: str  # 1-2 | 3-4 | 5-6 | 6+
+    type_of_search: str  # moderate | aggressive
+    police_badge: List[PoliceBadgeInfo]  # PoliceBadgeInfo
+    outcome: str  # unknown | ongoing | resolved | no further action
+    victim_description: str  # yes | no
+    age: str  # I don't know | below 15 | 15-17 | 18-24 | 25-30 | 31-35 | 35+
+    sex: str  # I don't know | male | female
+    race: str  # I don't know | black | white | arab | south asain | east asain
+    notes: str  # (optional)
+    supporting_evidence: List[UploadFile]  # (optional)
+    email: str  # (optional) - hashed for security
+
+
+class PartialWitnessReport(BaseModel):
+    location: Union[str, LocationInput]
+    victims_involved: str
+    visible_police: str
+    type_of_search: str
+    police_badge: List[PoliceBadgeInfo]
+    notes: str
+    supporting_evidence: List[UploadFile]
+
+"""
